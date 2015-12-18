9ab5947f71fcd13677b3a610ed75645efabf1461
Matthew Pickford <pickfordmatt@me.com>
2015-12-18 09:37:27 +0000

    Update prowl.py

--- prowl.py
+++ prowl.py
@@ -1,231 +1,112 @@
-#Importing Libs##########
 from bs4 import BeautifulSoup
 import urllib2
 import sys
 from itertools import izip
 import string
 import argparse
-import random
-import time
+import requests
 
-#########################
-
-URLS = []
-EMAILS = []
 GOOGLE = []
-
 
 parser = argparse.ArgumentParser(description="Scrape LinkedIn for staff members")
 parser.add_argument("-u", "--url", help="URL of a public profile to start a basic search from")
 parser.add_argument("-c", "--company", help="Company to search for")
-parser.add_argument("-o", "--outfile", help="Filename to write results to")
-parser.add_argument("-w", "--wait", help="Wait seconds between requests")
-parser.add_argument("-e", "--emailformat", help="Format of house email address style. Use: <fn>,<ln>,<fi>,<li> as placeholders for first/last name/initial. e.g <fi><ln>@company.com")
-parser.add_argument("-d", "--debug", action="store_true", help="Turn on debug output")
-parser.add_argument("-k", "--cookie", help="Values to send in Cookie: header to linkedin" )
 args = parser.parse_args()
 
-def deep_and_thorough( Org ):	
+URLS = []
+
+##########################
+
+def greppage(org, link):
+	#Setting User Agent#######
+	header = {'User-Agent': 'WatchFish'} #Needed to prevent 403 error on Wikipedia
+	##########################
+	#Making HTTP req##########
+	req = requests.get(link,headers=header)
+	page = req.text
+	soup = BeautifulSoup(page, "lxml")
+	for td in soup.findAll("li", { "class":"profile-card" }):
+		item = td.getText()
+		print td.find('a')['href']
+		URLS.append(td.find('a')['href'].lower())	 	
+		try:
+			for i in URLS:
+				#Setting User Agent#######
+				header = {'User-Agent': 'WatchFish'} #Needed to prevent 403 error on Wikipedia
+				##########################
+				#Making HTTP req##########
+				req = urllib2.Request(i,headers=header)
+				page = urllib2.urlopen(req)
+				soup = BeautifulSoup(page, "lxml")
+				for td in soup.findAll("li", { "class":"profile-card" }):
+					url = td.find('a')['href'].lower()
+					profile = td.getText().lower()
+					if org.lower() in profile:
+				     	 if url not in URLS:
+				    	 	URLS.append(url)
+				    	 	print profile + "          from           " + i    	 	
+				    	 	
+		except:
+			pass
+	print "\n".join(URLS)
+
+def deep_and_thorough(org):	
   global GOOGLE, EMAILS, URLS
-  Org2 = Org.lower()
-
-  if args.debug:
-    print "deep_and_thorough( "+Org+" )"
-
-  company2 = Org2.replace(" ", "")
+  company2 = org.lower()
+  company3 = company2.replace(" ", "+")
   #Setting User Agent#######
-  header = {'User-Agent': 'Mozilla/5.0'} 
+  header = {'User-Agent': 'Mozilla/5.0'} #Needed to prevent 403 error on Wikipedia
   ##########################
         
   #Making HTTP req##########
-  if args.debug:
-    print "Searching Yahoo..."
-  req2 = urllib2.Request("https://uk.search.yahoo.com/search?p="+company2+"%20linkedin%20")
-  page2 = urllib2.urlopen(req2)
-  if args.debug:
-    print "Page\n\n" + str( page2 )
-  soup2 = BeautifulSoup(page2, "lxml")
+  print "Searching Yahoo..."
+  req = urllib2.Request("https://uk.search.yahoo.com/search?p="+company3+"%20linkedin%20/pub/")
+  page = urllib2.urlopen(req)
+  soup = BeautifulSoup(page, "lxml")
   ##########################
 
-  company2 = soup2.findAll("h3", {"class" : "title"})
+  company = soup.findAll("h3", {"class" : "title"})
 
-  if args.debug:
-    print str(len(company2)) + " results found"
-
-  for i2 in company2:
-    c2 = i2.find("a")
-    if args.debug:
-      print c2['href']
-    GOOGLE.append(c2['href'])
+  for i in company:
+    c = i.find("a")
+    print c
+    GOOGLE.append(c['href'])
 
   ##########################
 
   print "---------------------------------------"
-  print "Searching for " + Org
+  print "Searching for " + org
   print "----------------------------------------"
 
   for link in GOOGLE:
     try:
-      #print link
       if "linkedin.com/pub/" or "linkedin.com/in/" in link:
-        greppage(link, Org)
-        for iturl in URLS:
-          greppage(iturl, Org)
+        #print link
+        URLS.append(link)
       else:
         print "non-company link"
-    except: "Yahoo dun goofed"    
+    except: "Yahoo dun goofed"  
 
-  GOOGLE = []
+  for i in URLS:
+  	try:
+		#Setting User Agent#######
+		header = {'User-Agent': 'WatchFish'} #Needed to prevent 403 error on Wikipedia
+		##########################
+		#Making HTTP req##########
+		req = urllib2.Request(i,headers=header)
+		page = urllib2.urlopen(req)
+		soup = BeautifulSoup(page, "lxml")
+		for td in soup.findAll("li", { "class":"profile-card" }):
+			url = td.find('a')['href']
+			profile = td.getText().lower()
+			if company3 in profile:
+		     	 if url not in URLS:
+		    	 	URLS.append(url)
+		    	 	print profile + "       " + url
+	except:
+		pass   
 
-def basic_search( Org, link ):
-  global GOOGLE, EMAILS, URLS
-  Org = Org.lower()
-
-  ##########################
-
-  print "---------------------------------------"
-  print "Searching for " + Org
-  print "----------------------------------------"
-
-  greppage(link, Org)
-
-  for iturl in URLS:
-    try:
-      greppage(iturl, Org)
-    except:
-      print "You dun goofed"
-
-def greppage(link, org ):
-  global GOOGLE, EMAILS, URLS, target, args
-  
-  if args.debug:
-    print "greppage( '"+link+"', '"+org+"')"
- 
-  ua1 = ['Fire', 'Ice', 'Magic', 'Slippery', 'Spectacular', 'Easy', 'Genius', 'Grumpy','Buttery','Spicey']
-  ua2 = ['fox','weasel','donkey','ocelot','stoat','rabbit','wombat','tapir','squirrel','otter','hedgehog']
-  useragent = random.choice(ua1) + random.choice(ua2) + " v" + str(random.randrange(0,60) + random.random())
-  if args.debug:
-    print "User agent: " + useragent
-
-  #Setting User Agent#######
-  header = {'User-Agent': 'Mozilla/5.0 ' + useragent}
-  if args.cookie:
-    header['Cookie'] = args.cookie
-  ##########################
-  
-  if args.debug:
-    print "Requesting linkedin profile"
-    print header
-  
-  #Making HTTP req##########
-  if args.wait:
-    if args.debug:
-      print "Delaying " + str( int(args.wait) ) + " seconds"
-    time.sleep(int(args.wait))
-  try:
-    req = urllib2.Request(link,headers=header)
-    page = urllib2.urlopen(req)
-  except Exception as e:
-    print "[WARNING] No page returned from " + link
-    print e
-    return 
-
-  soup = BeautifulSoup(page, "lxml")
- 
-  # Check this user, use the headline and current employment from the profile page
-  profile = soup.find("div", class_="profile-overview-content")
-  name = profile.find("h1", id="name" ).get_text()
-  company =  profile.find("p", class_="headline").get_text() + " - " + profile.find("span", class_="org" ).get_text()
-  if args.debug:
-    # print profile
-    print name
-    print company
-  
-  process_person( name, company, link, org )
-
-  cards = soup.find_all("li", class_="profile-card")
-  if args.debug:
-    print str(len(cards)) + " cards found"
-    # print cards
-    print "Looping through cards"
-
-  for card in cards:
-    # if args.debug:
-    #   print "Card:"
-    #   print card
-   
-    name = card.find("h4").find("a").get_text()
-    company = card.find("p", class_="headline").get_text()
-    url = card.find("a")["href"]
-    
-    process_person( name, company, url, org )
-
-
-def process_person( name, company, url, org ):
-  if args.debug:
-    print name
-    print company
-    print url
-    print "Testing if "+org.lower()+" is in \"" + company.lower() + "\""
-  if org.lower() in company.lower():
-    if args.debug:
-      print "Company identified in job title"
-    if url not in URLS:
-      if args.debug:
-        print "New URL found, adding"
-      URLS.append(url)
-      data = name + chr(9) + company + chr(9) + url
-      data = filter(lambda x: x in string.printable, data)
-      sys.stdout.write( data )
-      target.write( data + "\n" )
-      if args.emailformat:
-        fn = string.split(name)[0]
-        fi = fn[0]
-        ln = ''.join(string.split(name)[1:])
-        li = ln[1]
-        email = args.emailformat.replace('<fn>',fn).replace('<ln>',ln).replace('<fi>',fi).replace('<li>',li).lower()
-        email2 = filter(lambda x: x in string.printable, email)
-        sys.stdout.write( chr(9) + email2 )
-        f = open( outfile + '.emails', 'a' )
-        f.write( email2 + "\n" )
-      sys.stdout.write( "\n" )
-
-def mangle_emails(names, pattern, orgname):
-  global outfile
-  try:
-    for name in names:
-      fn = string.split(name)[0]
-      fi = fn[0]
-      ln = ''.join(string.split(name)[1:])
-      li = ln[1]
-      email = pattern.replace('<fn>',fn).replace('<ln>',ln).replace('<fi>',fi).replace('<li>',li).lower()
-      email2 = filter(lambda x: x in string.printable, email)
-      f = open( outfile + '.emails', 'a' )
-      f.write( email2 + "\n" )
-      
-
-
-  except:
-    print "oops you've got an error"
-
-if args.debug:
-  print args
-
-if args.company:
-  if args.outfile:
-    outfile = args.outfile
-  else:
-    outfile = args.company.replace(' ','_')+".output.txt"
-  target = open(outfile, 'a')
-  
-  # Basic quick search if there's a URL provided
-  if args.url:
-    basic_search(args.company,args.url) 
-  else:
-    deep_and_thorough(args.company)
-
-  if args.emailformat:
-    mangle_emails( EMAILS, args.emailformat, args.company ) 
-  sys.exit(0)
+if args.url:
+    greppage(args.company,args.url)
 else:
-  parser.print_usage()
+    deep_and_thorough(args.company)
