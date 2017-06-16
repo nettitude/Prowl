0a3b98e8b8a8b17b3c60063f47e2e9b1e724d558
Matthew Pickford <pickfordmatt@me.com>
2017-06-16 19:27:39 +0100

    Update prowl.py

--- prowl.py
+++ prowl.py
@@ -3,6 +3,14 @@
 import urllib2
 import os
 import git
+import argparse
+
+parser = argparse.ArgumentParser(description="Scrape LinkedIn for company staff members")
+parser.add_argument("-c", "--company", help="Company to search for")
+parser.add_argument("-e", "--emailformat", help='Format of house email address style. Use: <fn>,<ln>,<fi>,<li> as placeholders for first/last name/initial. e.g "<fi><ln>@company.com"')
+parser.add_argument("-nj", "--nojobs",  help="Include available jobs in result", action='store_true')
+parser.add_argument("-d", "--depth", help="How many pages of Yahoo to search through", default='10')
+args = parser.parse_args()
 
 def welcome():
         print "_"*50
@@ -21,7 +29,6 @@ def welcome():
         print "_"*50
         print ""
 welcome()
-
 
 response = urllib2.urlopen('https://raw.githubusercontent.com/pickfordmatt/Prowl/master/Version.txt')
 html = response.read()
