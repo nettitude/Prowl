c3848f8acddb8c67214d01d44f077e6b02786166
Matthew Pickford <pickfordmatt@me.com>
2017-07-24 15:40:59 +0100

    Update file.py

--- file.py
+++ file.py
@@ -32,16 +32,51 @@ class bcolors:
     CUSTOM = '\033[37m'
 
 def formatout(companyname,emailformat):
-        domain = emailformat.split("@")[1]
-        if emailformat:
-                print "Output file name: "+emailformat.split("@")[1]+".txt"
-        if not os.path.exists("Output"):
-                os.makedirs("Output")
+	domain = emailformat.split("@")[1]
+	if emailformat:
+		print "Output file name: "+companyname+".csv"
+	if not os.path.exists("Output"):
+		os.makedirs("Output")
 	print "_"*50
 	print " "
+	target = open("Output/"+companyname+".csv", 'w')
+	target.write("First Name, Last Name, Email, Title, Profile, Breach"+"\r\n")
 
-def mangle_emails(name, company, emailformat):
-	target = open("Output/"+company+".txt", 'a')
+def search(comp,emailformat):
+	depth = int(args.depth)
+	print "\n"
+	print bcolors.UNDERLINE + bcolors.WARNING + "S" + bcolors.OKBLUE + "T" + bcolors.OKGREEN + "A" + bcolors.ENDC + bcolors.UNDERLINE + "F" + bcolors.FAIL + "F" + bcolors.ENDC
+	print "\n"
+	for i in range(1,depth):
+		i = str(i)
+		r = requests.get('https://uk.search.yahoo.com/search;_ylt=A9mSs3IiEdVYsUMAY6ZLBQx.;_ylu=X3oDMTEzdm1nNDAwBGNvbG8DaXIyBHBvcwMxBHZ0aWQDBHNlYwNwYWdpbmF0aW9u?p="at+{0}"+site%3Alinkedin.com&pz=10&ei=UTF-8&fr=yfp-t-UK317&b={1}0&pz=10&xargs=0'.format(comp,i))
+		soup = BeautifulSoup(r.text, "lxml")
+        	for tag in soup.findAll("div", {"class" : "dd algo algo-sr Sr"}):
+			if re.search("linkedin.com/in/", str(tag)):
+				if re.search("Top", tag.getText()):
+					pass
+				else:
+					if tag.getText() not in found:
+						title = tag.find("h3", {"class" : "title"});
+						div = " "
+						cut = title.getText().split(div,2)[:2]
+						name = cut[0]+" "+cut[1]
+						jobtext = tag.find("p", {"class" : "lh-16"});
+						job = jobtext.getText()
+						href = tag.find("a");
+						linkpof = ""
+						linkpof = href['href']
+						found.append(tag.getText())
+						if "View " in job:
+							fulljob = ""
+							mangle_emails(name, comp, emailformat, fulljob, linkpof)
+						else:
+							first = job.split(".",1)[1]
+							fulljob = first.split(" at ",1)[0]
+							mangle_emails(name, comp, emailformat, fulljob, linkpof)
+
+def mangle_emails(name, company, emailformat, fulljob, linkpof):
+	target = open("Output/"+company+".csv", 'a')
         fn = string.split(name)[0]
         fi = fn[0]
         ln = string.split(name)[1]
@@ -58,42 +93,17 @@ def mangle_emails(name, company, emailformat):
 			emails.append(email2)
 			req = requests.get("https://haveibeenpwned.com/api/breachedaccount/"+email2+"?truncateResponse=true", headers=headers)
 			try:
-				print "{0:30} {1:40} {2}".format(name, email2, req.content)
-				target.write(email2+"\r\n")
-                	except:
+				breach = str(req.content)
+				print "{0:30} {1:40} {3:40} {2}".format(name, email2, req.content, fulljob)
+				target.write(fn+","+ln+","+email2+","+fulljob+","+linkpof+","+breach+"\r\n")
+			except:
                         	pass
         	except:
 			emails.append(email2)
 			print "{0:20} {1}".format(name,email2)
 			print "fail"
-			target.write(email2+"\r\n")
 	else:
 		pass
-
-def search(comp,emailformat):
-	depth = int(args.depth)
-	print "\n"
-	print bcolors.UNDERLINE + bcolors.WARNING + "S" + bcolors.OKBLUE + "T" + bcolors.OKGREEN + "A" + bcolors.ENDC + bcolors.UNDERLINE + "F" + bcolors.FAIL + "F" + bcolors.ENDC
-	print "\n"
-	for i in range(1,depth):
-		i = str(i)
-		r = requests.get('https://uk.search.yahoo.com/search;_ylt=A9mSs3IiEdVYsUMAY6ZLBQx.;_ylu=X3oDMTEzdm1nNDAwBGNvbG8DaXIyBHBvcwMxBHZ0aWQDBHNlYwNwYWdpbmF0aW9u?p="at+{0}"+site%3Alinkedin.com&pz=10&ei=UTF-8&fr=yfp-t-UK317&b={1}0&pz=10&xargs=0'.format(comp,i))
-		soup = BeautifulSoup(r.text, "lxml")
-        	for tag in soup.findAll("h3", {"class" : "title"}):
-			if re.search("linkedin.com/in/", str(tag)):
-				if re.search("Top", tag.getText()):
-					pass
-				else:
-					if tag.getText() not in found:
-						div = '|'
-						cut = tag.getText().split(div,1)[0]
-						mangle_emails(cut, comp, emailformat)
-						found.append(tag.getText())
-					else:
-						pass
-			else:
-				pass
-
 
 def jobs(comp):
 	print bcolors.UNDERLINE + bcolors.WARNING + "J" + bcolors.OKBLUE + "O" + bcolors.OKGREEN + "B" + bcolors.ENDC + bcolors.UNDERLINE + "S"  + bcolors.ENDC
@@ -101,8 +111,8 @@ def jobs(comp):
 	r = requests.get("https://www.indeed.co.uk/jobs?as_and='at+{0}'&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=30&sort=&psf=advsrch".format(comp))
 	soup = BeautifulSoup(r.text, "lxml")
 	for tag in soup.findAll("h2", {"class" : "jobtitle"}):
-		print tag.getText().strip()
-
+		#print str(tag.getText()).strip()
+		pass
 if args.company:
 	if args.emailformat:
 		formatout(args.company, args.emailformat)
