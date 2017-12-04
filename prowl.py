f6f987bc494ba291059108c0a7c736804c0fb0cc
Matthew Pickford <3343862+Pickfordmatt@users.noreply.github.com>
2017-12-04 13:52:47 +0000

    Update prowl.py

--- prowl.py
+++ prowl.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-import urllib2
 import os
 import git
 import argparse
+import requests
 
 parser = argparse.ArgumentParser(description="Scrape LinkedIn for company staff members")
 parser.add_argument("-c", "--company", help="Company to search for")
 parser.add_argument("-e", "--emailformat", help='Format of house email address style. Use: <fn>,<ln>,<fi>,<li> as placeholders for first/last name/initial. e.g "<fi><ln>@company.com"')
-parser.add_argument("-nj", "--nojobs",  help="Include available jobs in result", action='store_true')
+parser.add_argument("-j", "--jobs",  help="Include available jobs in result", action='store_true')
+parser.add_argument("-s", "--subdomains",  help="Find subdomains", action='store_true')
+parser.add_argument("-a", "--all",  help="FIND EVERYTHING", action='store_true')
 parser.add_argument("-d", "--depth", help="How many pages of Yahoo to search through", default='10')
+parser.add_argument("-p", "--proxy", help="http(s)://localhost:8080", default='')
 args = parser.parse_args()
 
 def welcome():
@@ -30,8 +33,8 @@ def welcome():
         print ""
 welcome()
 
-response = urllib2.urlopen('https://raw.githubusercontent.com/pickfordmatt/Prowl/master/Version.txt')
-html = response.read()
+response = requests.get('https://raw.githubusercontent.com/pickfordmatt/Prowl/master/Version.txt')
+html = response.text
 openfile = open('Version.txt', 'r')
 cversion = openfile.readline()
 
